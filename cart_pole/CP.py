a49d4e81edfadb30a18ce4f5857db4e16df24eb3
Alagris <alagris12358@gmail.com>
2021-03-22 18:18:52 +0100

    cart pole solved

--- cart_pole/CP.py
+++ cart_pole/CP.py
@@ -11,7 +11,7 @@ GAMMA = 1  # future reward discounting
 SAMPLES = []
 EPSILON_DECAY = 0.999
 N_EPISODES = 10000
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 STATE_FEATURE_DIMENSIONALITY = 4
 
 
@@ -19,17 +19,22 @@ class DQN(torch.nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(DQN, self).__init__()
         self.linear1 = torch.nn.Linear(input_size, hidden_size)
-        self.linear2 = torch.nn.Linear(hidden_size, output_size)
+        self.linear2 = torch.nn.Linear(hidden_size, hidden_size)
+        self.linear3 = torch.nn.Linear(hidden_size, hidden_size)
+        self.linear4 = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        h_relu = self.linear1(x).clamp(min=0)
-        y_pred = self.linear2(h_relu)
+        # torch.relu()
+        h_relu1 = self.linear1(x).clamp(min=0)
+        h_relu2 = self.linear2(h_relu1).clamp(min=0)
+        h_relu3 = self.linear3(h_relu2).clamp(min=0)
+        y_pred = self.linear4(h_relu3)
         return y_pred
 
 
-MODEL = DQN(STATE_FEATURE_DIMENSIONALITY, 4, 2)
+MODEL = DQN(STATE_FEATURE_DIMENSIONALITY, 16, 2)
 LOSS_CRITERION = torch.nn.MSELoss()
-OPTIMIZER = torch.optim.Adam(MODEL.parameters())
+OPTIMIZER = torch.optim.Adam(MODEL.parameters(), weight_decay=0.01)
 
 # episode = []
 # observation = torch.tensor(env.reset())
@@ -66,24 +71,23 @@ for e in range(N_EPISODES):
             total_reward += reward
         total_reward_per_episode.append(total_reward)
         epsilon_per_episode.append(EPSILON*100)
-        print("reward = ", total_reward)
         plt.clf()
         plt.plot(total_reward_per_episode)
         plt.plot(epsilon_per_episode)
         plt.pause(interval=0.0001)
 
-        x_batch = torch.zeros((BATCH_SIZE, STATE_FEATURE_DIMENSIONALITY))
-        y_batch = torch.zeros((BATCH_SIZE, 2))
+        if i < 499:
+            x_batch = torch.zeros((BATCH_SIZE, STATE_FEATURE_DIMENSIONALITY))
+            y_batch = torch.zeros((BATCH_SIZE, 2))
 
-        minibatch = random.sample(MEMORY, min(len(MEMORY), BATCH_SIZE))
-
-        for i, (state, action, reward, next_state, done) in enumerate(minibatch):
-            y_batch[i] = MODEL(state)
-            y_batch[i, action] = reward if done else reward + GAMMA * max(MODEL(next_state))
-            x_batch[i] = state
-
-    predictions = MODEL(x_batch)
-    loss = LOSS_CRITERION(predictions, y_batch)
-    loss.backward()
-    OPTIMIZER.step()
-    OPTIMIZER.zero_grad()
+            minibatch = random.sample(MEMORY, min(len(MEMORY), BATCH_SIZE))
+            for i, (state, action, reward, next_state, done) in enumerate(minibatch):
+                y_batch[i] = MODEL(state)
+                y_batch[i, action] = reward if done else reward + GAMMA * max(MODEL(next_state))
+                x_batch[i] = state
+    if i < 499:
+        predictions = MODEL(x_batch)
+        loss = LOSS_CRITERION(predictions, y_batch)
+        loss.backward()
+        OPTIMIZER.step()
+        OPTIMIZER.zero_grad()
