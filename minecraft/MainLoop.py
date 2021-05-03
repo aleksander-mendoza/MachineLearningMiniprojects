ab4df214a0be0b4edc3d68d6caf03a81f36f326a
Aleksander Mendoza <aleksander.mendoza.drosik@gmail.com>
2021-05-03 14:46:18 +0200

    small updates

--- minecraft/MainLoop.py
+++ minecraft/MainLoop.py
@@ -4,16 +4,24 @@ os.putenv("JAVA_HOME", "/usr/lib/jvm/java-8-openjdk-amd64")
 
 import minerl
 import gym
-import logging
+env = gym.make('MineRLNavigateDense-v0')
 
-logging.basicConfig(level=logging.DEBUG)
-
-env = gym.make("MineRLTreechop-v0")
 
 obs = env.reset()
-
 done = False
+net_reward = 0
 
 while not done:
-    action = env.action_space.sample()
-    obs, reward, done, _ = env.step(action)
+    action = env.action_space.noop()
+
+    action['camera'] = [0, 0.03*obs["compassAngle"]]
+    action['back'] = 0
+    action['forward'] = 1
+    action['jump'] = 1
+    action['attack'] = 1
+
+    obs, reward, done, info = env.step(
+        action)
+
+    net_reward += reward
+    print("Total reward: ", net_reward)
