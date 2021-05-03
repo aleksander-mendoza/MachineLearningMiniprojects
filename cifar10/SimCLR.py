8f1783badc75de8b6e57a35a7dca80e4bc211448
Aleksander Mendoza <aleksander.mendoza.drosik@gmail.com>
2021-05-03 14:41:25 +0200

    CimCLR

--- cifar10/SimCLR.py
+++ cifar10/SimCLR.py
@@ -7,19 +7,22 @@ import matplotlib.pyplot as plt
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as F2
+from tqdm import tqdm
+
 
 DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 crop = transforms.RandomCrop(32, padding=4)
 flip = transforms.RandomHorizontalFlip()
-h = 32
-w = 32
-c = 3
+height = 32
+width = 32
+channels = 3
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True)
 data = torch.from_numpy(trainset.data).float()
+data = data
 data.div_(255)
 print("SHAPE = (B, W, H, C)")
 print("SHAPE =", data.size())
@@ -34,8 +37,8 @@ print("STD =", std)
 data = data.transpose(1, 3)
 print("SHAPE = (B, C, H, W)")
 print("SHAPE =", data.size())
-trainloader = torch.utils.data.DataLoader(
-    data, batch_size=4, shuffle=True, num_workers=2)
+BATCH_SIZE = 512
+trainloader = torch.utils.data.DataLoader(data, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
 
 
 # testset = torchvision.datasets.CIFAR10(
@@ -43,29 +46,38 @@ trainloader = torch.utils.data.DataLoader(
 # testloader = torch.utils.data.DataLoader(
 #     testset, batch_size=100, shuffle=False, num_workers=2)
 
-class CNN(torch.nn.Module):
-
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=4, kernel_size=(3, 3))
-        self.conv2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(3, 3))
-        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3))
-        self.conv4 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3))
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x), True)
-        x = F.relu(self.conv2(x), True)
-        x = F.relu(self.conv3(x), True)
-        x = F.relu(self.conv4(x), True)
-        return x
+# class CNN(torch.nn.Module):
+#
+#     def __init__(self):
+#         super(CNN, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=(5, 5))
+#         self.conv2 = nn.Conv2d(in_channels=6, out_channels=8, kernel_size=(5, 5))
+#         self.conv3 = nn.Conv2d(in_channels=8, out_channels=10, kernel_size=(5, 5))
+#         self.conv4 = nn.Conv2d(in_channels=10, out_channels=12, kernel_size=(5, 5))
+#         self.conv5 = nn.Conv2d(in_channels=12, out_channels=14, kernel_size=(5, 5))
+#         self.conv6 = nn.Conv2d(in_channels=14, out_channels=16, kernel_size=(5, 5))
+#         self.conv7 = nn.Conv2d(in_channels=16, out_channels=18, kernel_size=(5, 5))
+#         self.lin = nn.Linear(18*4*4, 256)
+#
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x), True)
+#         x = F.relu(self.conv2(x), True)
+#         x = F.relu(self.conv3(x), True)
+#         x = F.relu(self.conv4(x), True)
+#         x = F.relu(self.conv5(x), True)
+#         x = F.relu(self.conv6(x), True)
+#         x = F.relu(self.conv7(x), True)
+#         x = x.reshape(BATCH_SIZE*2, 18*4*4)
+#         x = F.relu(self.lin(x), True)
+#         return x
 
 
 class Head(torch.nn.Module):
 
     def __init__(self):
         super(Head, self).__init__()
-        self.lin1 = nn.Linear(32, 8)
-        self.lin2 = nn.Linear(8, 8)
+        self.lin1 = nn.Linear(128, 128)
+        self.lin2 = nn.Linear(128, 128)
 
     def forward(self, x):
         x = F.relu(self.lin1(x), True)
@@ -73,17 +85,17 @@ class Head(torch.nn.Module):
         return x
 
 
-model = CNN().to(DEVICE)
+model = torchvision.models.resnet18(pretrained=False, num_classes=128).to(DEVICE)
 head = Head().to(DEVICE)
 
 
 def augmentations(img):
     crop_h = 16
     crop_w = 16
-    i = torch.randint(h - crop_h + 1, size=(1,)).item()
-    j = torch.randint(w - crop_w + 1, size=(1,)).item()
+    i = torch.randint(height - crop_h + 1, size=(1,)).item()
+    j = torch.randint(width - crop_w + 1, size=(1,)).item()
     img = F2.crop(img, i, j, crop_h, crop_w)
-    img = F2.resize(img, (h, w))
+    img = F2.resize(img, (height, width))
     fn_idx = torch.randperm(4)
     for fn_id in fn_idx:
         if fn_id == 0 and torch.rand(1) < 0.2:
@@ -107,21 +119,72 @@ def augmentations(img):
     return img
 
 
-VISUALIZE_AUGMENTATIONS = 1
-
-for epoch in range(100):
+optim = torch.optim.Adam(model.parameters(), lr=0.00001)
+VISUALIZE_AUGMENTATIONS = 0
+criterion = torch.nn.CrossEntropyLoss()
+labels = torch.cat([torch.arange(BATCH_SIZE) for i in range(2)], dim=0)
+labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).float()
+labels = labels.to(DEVICE)
+mask = torch.eye(labels.shape[0], dtype=torch.bool).to(DEVICE)
+labels = labels[~mask].view(labels.shape[0], -1)
+zeros = torch.zeros(BATCH_SIZE*2, dtype=torch.long).to(DEVICE)
+TEMPERATURE = 0.05
+EPOCHS = 1000000
+epoch_bar = tqdm(total=EPOCHS, position=1, desc="Epoch")
+batch_bar = tqdm(total=len(data), position=2)
+losses = []
+for epoch in range(EPOCHS):
+    total_loss = 0
+    batch_bar.reset()
+    if VISUALIZE_AUGMENTATIONS == 0:
+        plt.clf()
+        plt.plot(losses)
+        plt.pause(interval=0.001)
     for batch in trainloader:
         x1 = augmentations(batch)
         x2 = augmentations(batch)
         if VISUALIZE_AUGMENTATIONS > 0:
             plt.clf()
             plt.subplot(1, 3, 1)
-            plt.imshow((batch.transpose(3, 1).reshape(-1, w, c)))
+            plt.imshow((batch.transpose(3, 1).reshape(-1, width, channels)))
             plt.subplot(1, 3, 2)
-            plt.imshow((x2.transpose(3, 1).reshape(-1, w, c)))
+            plt.imshow((x2.transpose(3, 1).reshape(-1, width, channels)))
             plt.subplot(1, 3, 3)
-            plt.imshow((x1.transpose(3, 1).reshape(-1, w, c)))
+            plt.imshow((x1.transpose(3, 1).reshape(-1, width, channels)))
             plt.pause(interval=VISUALIZE_AUGMENTATIONS)
 
-        # x1 = model(x1)
-        # x2 = model(x2)
+        x = torch.cat([x1, x2])
+        x = x.to(DEVICE)
+        h = model(x)
+        h = h.reshape(BATCH_SIZE*2, -1)
+        z = head(h)
+
+        s = F.normalize(z, dim=1)
+        cosine_similarity_matrix = torch.matmul(s, s.T)
+
+        similarity_matrix = cosine_similarity_matrix[~mask].view(BATCH_SIZE*2, BATCH_SIZE*2 - 1)
+        # assert similarity_matrix.shape == labels.shape
+
+        # select and combine multiple positives
+        positives = similarity_matrix[labels.bool()].view(BATCH_SIZE*2, 1)
+
+        # select only the negatives the negatives
+        negatives = similarity_matrix[~labels.bool()].view(BATCH_SIZE*2, BATCH_SIZE*2-2)
+
+        logits = torch.cat([positives, negatives], dim=1)
+
+        logits = logits / TEMPERATURE
+        loss = criterion(logits, zeros)
+        total_loss += loss.item()
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        batch_bar.set_description("Loss "+str(loss.item()))
+        batch_bar.update(BATCH_SIZE)
+    total_loss = BATCH_SIZE*2*total_loss/len(data)
+    losses.append(total_loss)
+    epoch_bar.update(1)
+    print("total loss=", total_loss)
+
+
+
--- temporal_classification_polish/BERT.py
+++ temporal_classification_polish/BERT.py
@@ -1,54 +1,90 @@
 # data from  https://git.wmi.amu.edu.pl/kubapok/retroc2
 
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertForSequenceClassification, BertTokenizer, BertModel
 from transformers import pipeline
-from transformers import XLMTokenizer, RobertaModel
+from transformers import XLMTokenizer, RobertaModel, AutoModel, AutoTokenizer
 import torch
 import os
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from transformers.tokenization_utils_base import TruncationStrategy
 
 if not os.path.isdir('retroc2'):
     print('First run\ngit clone https://git.wmi.amu.edu.pl/kubapok/retroc2')
     exit()
 
-tokenizer = XLMTokenizer.from_pretrained("allegro/herbert-klej-cased-tokenizer-v1")
-model = RobertaModel.from_pretrained("allegro/herbert-klej-cased-v1")
+DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+allegro = False
+if allegro:
+    tokenizer = XLMTokenizer.from_pretrained("allegro/herbert-klej-cased-tokenizer-v1")
+    model = RobertaModel.from_pretrained("allegro/herbert-klej-cased-v1")
+else:
+    model = BertModel.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
+    tokenizer = BertTokenizer.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
 
+max_len = 512
+out_len = 768
+model = model.to(DEVICE)
 data_dir = 'retroc2/train'
-top = torch.nn.Linear(768, 1)
+top = torch.nn.Linear(out_len, 1).to(DEVICE)
 dataset = []
 with open(data_dir + '/train.tsv') as t:
     for data in t:
         data = data.strip().split('\t')
-        year = data[0]
+        year = float(data[0])
         text = data[4]
-        dataset.append((year, text))
+        dataset.append([text, year])
 
+year_tensor = torch.tensor(list(map(lambda x:x[1], dataset)))
+year_std, year_mean = torch.std_mean(year_tensor)
+year_std = year_std.item()
+year_mean = year_mean.item()
+year_variance = year_mean*year_mean
+for text_year in dataset:
+    text_year[1] = (text_year[1]-year_mean)/year_std
+del year_tensor
 
 def collate(b):
     sentences = [sentence for sentence, _ in b]
     years = torch.tensor([year for _, year in b])
     years = years.unsqueeze(1)
-    sentences = tokenizer.encode(sentences, return_tensors='pt')
-    return sentences, years
+    encoded = tokenizer(sentences, return_tensors='pt', padding=True, truncation=True, max_length=max_len)
+    return encoded, years
 
 
-trainloader = torch.utils.data.DataLoader(dataset, collate=collate, batch_size=4, shuffle=True)
+EPOCHS = 2
+epoch_bar = tqdm(total=EPOCHS, position=1)
+batch_bar = tqdm(total=len(dataset), position=2)
+BATCH=4
+trainloader = torch.utils.data.DataLoader(dataset, collate_fn=collate, batch_size=BATCH, shuffle=True)
 optimizer = torch.optim.Adam(top.parameters())
 criterion = torch.nn.MSELoss()
 losses = []
-for epoch in range(2):
-    total_loss = 0
+for epoch in range(EPOCHS):
+    total_mse = 0
+    batch_bar.reset()
     for sentences, years in trainloader:
-        outputs = model(sentences)
-        predicted = top(outputs)
-        loss = criterion(outputs, predicted)
-        total_loss += loss.item()
-        top.zero_grad()
+        years = years.to(DEVICE)
+        input_ids = sentences['input_ids'].to(DEVICE)
+        attention_mask = sentences['attention_mask'].to(DEVICE)
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=attention_mask)
+        predicted = top(outputs.pooler_output)
+        loss = criterion(years, predicted)
+        # mse == (years*year_std+year_mean - (predicted*year_std+year_mean))^2 / BATCH
+        # mse == (years*year_std - predicted*year_std)^2 / BATCH
+        # mse == (years - predicted)^2*year_std^2 / BATCH
+        # mse == (years - predicted)^2 / BATCH *year_variance
+        # mse == loss * year_variance
+        total_mse = loss.item()*year_variance
+        optimizer.zero_grad()
         loss.backward()
-        top.step()
-    losses.append(total_loss)
+        optimizer.step()
+        batch_bar.set_description("mse "+str(total_mse))
+        batch_bar.update(BATCH)
+    total_mse /= len(dataset)/BATCH
+    losses.append(total_mse)
     plt.clf()
     plt.plot(losses)
     plt.pause(interval=0.001)
+    epoch_bar.update(1)
